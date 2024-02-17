27c682cd9f02fe4a072e2bac803379efe8976aae
Andrej <dev@tualok.net>
2024-02-17 12:10:59 +0100

    add vars.tf

---
+++ vars.tf
@@ -0,0 +1,27 @@
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SECRET_KEY" {
+  type = string
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "S3_ENDPOINT" {
+  type = string
+}
+
+variable "STACK_NAME" {
+  type = string
+}
+
+variable "KUBE_CONTEXT" {
+  type = string
+}
+
+variable "KUBE_CONFIG_PATH" {
+  type = string
+}
